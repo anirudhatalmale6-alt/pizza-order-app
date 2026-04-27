77142da32a79dfd7fdf95be1650017ba5a86ea24
Anirudha Talmale <anirudhatalmale6@gmail.com>
2026-04-27 14:20:33 +0000

    Add seller name field on front screen, include in order messages

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"18b71d647a292a980abb405ac7d16fe1f0b204
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3719581638"
+    serviceWorkerVersion: "2827831014"
   }
 });
